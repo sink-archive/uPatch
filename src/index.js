ec06fb3a7cf44f441faba0f4a272ffc73dfa9a2e
Cain Atkinson <yellowsink@protonmail.com>
2021-08-28 10:20:10 +0100

    can get all files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 // @flow
 
-const { args }       = require('./args.js');
-const { getFilelist } = require('./filelists.js')
+const { args } = require("./args.js");
+const { getFilelist } = require("./filelists.js");
+const path = require("path");
 
 function main(): void {
-    if (args._[0] == "gen") {
-        gen(args.sourcedir, args.destdir, args['destoffset(optional)']);
-    }
-    else if (args._[0] == "apply") {
-        apply(args.sourcedir, args.destdir);
-    }
+  if (args._[0] == "gen") {
+    gen(args.sourcedir, args.destdir, args["destoffset(optional)"]);
+  } else if (args._[0] == "apply") {
+    apply(args.sourcedir, args.destdir);
+  }
 }
 
 function gen(sourceDir: string, destDir: string, offsetDir: string) {
-    console.log(getFilelist(sourceDir));
-    console.log(getFilelist(destDir));
+  console.log(getFilelist(sourceDir));
+  console.log(getFilelist(applyOffsetIfNecessary(destDir, offsetDir)));
 }
 
-function apply(sourceDir: string, destDir: string) {
-
+function applyOffsetIfNecessary(destDir: string, offsetDir: string) {
+    return offsetDir != undefined ? path.join(destDir, offsetDir) : destDir;
 }
 
+function apply(sourceDir: string, destDir: string) {}
 
 main(); // why node this isnt python have a main function like a sensible lang
