d29fbf2cfb37b45ab0e5cb58a57bc88d493e3847
Cain Atkinson <yellowsink@protonmail.com>
2021-08-28 09:58:06 +0100

    command parsing stuff actually working

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@
 const { args } = require('./args.js');
 
 function main(): void {
+    if (args._[0] == "gen") {
+        gen(args.sourcedir, args.destdir, args['destoffset(optional)']);
+    }
+    else if (args._[0] == "apply") {
+        apply(args.sourcedir, args.destdir);
+    }
+}
+
+function gen(sourceDir: string, destDir: string, offsetDir: string) {
     
 }
 
+function apply(sourceDir: string, destDir: string) {
 
-
+}
 
 
 main(); // why node this isnt python have a main function like a sensible lang
